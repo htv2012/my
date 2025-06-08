0c49483a47c6c05eba6680223aca45159efab601
Hai Vu <haivu2004@gmail.com>
2025-06-08 09:25:11 -0700

    Only create click shell completion when the tools exists

--- startup/click.sh
+++ startup/click.sh
@@ -1,2 +1,9 @@
-eval "$(_PASTEBIN_CLI_COMPLETE=zsh_source pastebin-cli)"
-eval "$(_PINBOARD_CLI_COMPLETE=zsh_source pinboard-cli)"
+if cmd_found pastebin-cli
+then
+    eval "$(_PASTEBIN_CLI_COMPLETE=zsh_source pastebin-cli)"
+fi
+if cmd_found pinboard-cli
+then
+    eval "$(_PINBOARD_CLI_COMPLETE=zsh_source pinboard-cli)"
+fi
+
