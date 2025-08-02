d9bb05885a79ee794a072f6c0b0bb04ec9180df0
Hai Vu <haivu2004@gmail.com>
2025-08-01 19:58:10 -0700

    Add git abbr to fish

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,4 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
+
+    # git
+    abbr --add ga 'git add'
+    abbr --add gb 'git branch'
+    abbr --add gc 'git commit'
+    abbr --add gcmsg 'git commit -m'
+    abbr --add gco 'git checkout'
+    abbr --add gd 'git diff'
+    abbr --add gl 'git pull'
+    abbr --add gp 'git push'
+    abbr --add gs 'git status'
 end
