ba582c97dc98465a3f4de52f9ac876c08d56277c
Hai Vu <haivu2004@gmail.com>
2025-11-14 21:09:35 -0800

    feat: Add make_project()

--- startup/python.sh
+++ startup/python.sh
@@ -18,6 +18,19 @@ alias pypath='echo $PYTHONPATH | tr : \\n'
 alias venv-clean="rm -fr ${VENV_ROOT}"
 
 
+make-project() {
+    uv init "$@"
+    projectDir="${@: -1}"
+    echo "projectDir=$projectDir"
+    cd "$projectDir" || exit
+    copier copy \
+        -d script=main.py \
+        -d default_target=run \
+        ~/my/copier-templates/uv-package-makefile .
+    uv add --dev ruff pytest ipython
+}
+
+
 nbclean() {  # Clean the Untitled* notebooks
     for root in $HOME/Projects/python_notebooks $HOME/JupyterNotebooks
     do
@@ -40,73 +53,5 @@ fi
 function pyclean() { # Cleans up all python-generated files
     find . -name '*.pyc' -delete
     find . -name '__pycache__' -delete
-}
-
-function mkpy(){ # Creates a python project with Makefile
-    mkdir "$1"
-    cd "$1" || exit
-    mkmake
-}
-
-function sandbox() { # Activate the sandbox virtualenv
-    venv ${VENV_ROOT}/sandbox $HOME/my/etc/requirements-sandbox.txt
-}
-
-
-pipw() {  # Pip wrapper: use uv if available
-    if cmd_found uv
-    then
-        uv pip "${@}"
-    else
-        pip install --upgrade pip
-        pip "${@}"
-    fi
-}
-
-
-pf() {  # pip freeze, with grep
-    pipw freeze | grep "${@:-.}"
-}
-
-fulfill() {  # Fulfills the requirements.txt
-    requirements="${1:-requirements.txt}"
-    pipw install --requirement "$requirements"
-}
-
-require() { # Installs the required packages
-    tr ' ' '\n' <<< "$@" >> requirements.txt
-    tmp_file=$(mktemp)
-    sort requirements.txt > "$tmp_file"
-    mv "$tmp_file" requirements.txt
-    fulfill requirements.txt
-}
-
-venv() {  # Sets up a virtual environment
-    deactivate > /dev/null 2> /dev/null
-    venv_dir="${1:-${VENV_ROOT}/$(basename $PWD)}"
-    requirements_file="${2:-requirements.txt}"
-
-    if [[ ! -d $venv_dir ]]
-    then
-        if cmd_found uv
-        then
-            uv venv --python python3 $venv_dir
-        else
-            python3 -m venv $venv_dir
-        fi
-
-        source $venv_dir/bin/activate
-
-        if [ -e $requirements_file ]
-        then
-            echo "Install requirements"
-            fulfill "$requirements_file"
-        else
-            echo "No requirements found"
-        fi
-
-        deactivate
-    fi
-    source $venv_dir/bin/activate
 }
 
