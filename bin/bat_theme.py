929702c6178413363d4bf0886b49d8cd4b76602b
Hai Vu <haivu2004@gmail.com>
2025-12-21 19:38:02 -0800

    feat: Add a 'use' option to bat_theme.py

--- bin/bat_theme.py
+++ bin/bat_theme.py
@@ -1,26 +1,50 @@
 #!/usr/bin/env python3
 """Show the bat themes."""
 
+import fileinput
 import shutil
 import subprocess
 
 
+def get_bat_config_path():
+    proc = subprocess.run([bat_exe, "--config-file"], text=True, capture_output=True)
+    return proc.stdout.strip()
+
+
+def use_theme(theme: str):
+    config_path = get_bat_config_path()
+    for line in fileinput.input(config_path, inplace=True):
+        if "--theme" in line:
+            line = f"--theme='{theme}'\n"
+        print(line, end="")
+
+
 def main():
-    bat = shutil.which("bat") or shutil.which("batcat")
-    if bat is None:
+    bat_exe = shutil.which("bat") or shutil.which("batcat")
+    if bat_exe is None:
         raise SystemExit("bat or batcat not found")
 
-    cmd = subprocess.run([bat, "--list-themes"], text=True, capture_output=True)
+    # Take a short sample of this file
+    with open(__file__, "r") as stream:
+        sample = stream.read(500)
+
+    # Get a list of themes
+    cmd = subprocess.run([bat_exe, "--list-themes"], text=True, capture_output=True)
     themes = cmd.stdout.splitlines()
 
+    # Show each theme
     for theme in themes:
         subprocess.run(["clear"], shell=True)
         print(f"Theme: {theme}\n")
-        subprocess.run([bat, "--theme", theme, __file__])
+        cmd = [bat_exe, f"--theme={theme}", "--language=python"]
+        subprocess.run(cmd, text=True, input=sample)
 
         print("\n")
-        user_input = input("Hit Enter or q to quit:")
+        user_input = input("Hit Enter or q to quit, u to use:").strip().lower()
         if user_input == "q":
+            break
+        elif user_input == "u":
+            use_theme(theme)
             break
 
 
