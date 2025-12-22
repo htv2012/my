4bb6ef256e326da629140f586f52a0443e8544c2
Hai Vu <haivu2004@gmail.com>
2025-12-22 08:44:15 -0800

    fix: multiple issues in bat_theme.py
    - fix: config file not found
    - fix: theme option not found in config file
    - fix: static analysis issues

--- bin/bat_theme.py
+++ bin/bat_theme.py
@@ -2,21 +2,38 @@
 """Show the bat themes."""
 
 import fileinput
+import pathlib
 import shutil
 import subprocess
 
 
-def get_bat_config_path():
+def get_bat_config_path(bat_exe: str):
     proc = subprocess.run([bat_exe, "--config-file"], text=True, capture_output=True)
     return proc.stdout.strip()
 
 
-def use_theme(theme: str):
-    config_path = get_bat_config_path()
+def ensure_config_file_exists(config_path: str):
+    path = pathlib.Path(config_path)
+    path.parent.mkdir(exist_ok=True)
+    path.touch(exist_ok=True)
+
+
+def use_theme(bat_exe: str, theme: str):
+    config_path = get_bat_config_path(bat_exe)
+    ensure_config_file_exists(config_path)
+
+    found = False
+    theme_option = f"--theme='{theme}'\n"
     for line in fileinput.input(config_path, inplace=True):
-        if "--theme" in line:
-            line = f"--theme='{theme}'\n"
+        if "--theme" in line and not line.strip().startswith("#"):
+            found = True
+            line = theme_option
         print(line, end="")
+
+    if not found:
+        with open(config_path, "a") as stream:
+            _ = stream.write("\n")
+            _ = stream.write(theme_option)
 
 
 def main():
@@ -24,27 +41,29 @@ def main():
     if bat_exe is None:
         raise SystemExit("bat or batcat not found")
 
-    # Take a short sample of this file
-    with open(__file__, "r") as stream:
-        sample = stream.read(500)
-
     # Get a list of themes
     cmd = subprocess.run([bat_exe, "--list-themes"], text=True, capture_output=True)
     themes = cmd.stdout.splitlines()
 
     # Show each theme
     for theme in themes:
-        subprocess.run(["clear"], shell=True)
+        _ = subprocess.run(["clear"], shell=True)
         print(f"Theme: {theme}\n")
-        cmd = [bat_exe, f"--theme={theme}", "--language=python"]
-        subprocess.run(cmd, text=True, input=sample)
+        cmd = [
+            bat_exe,
+            f"--theme={theme}",
+            "--language=python",
+            "--line-range=:25",
+            __file__,
+        ]
+        _ = subprocess.run(cmd)
 
         print("\n")
         user_input = input("Hit Enter or q to quit, u to use:").strip().lower()
         if user_input == "q":
             break
         elif user_input == "u":
-            use_theme(theme)
+            use_theme(bat_exe, theme)
             break
 
 
