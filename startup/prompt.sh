661c422b3524a02bb41833cca2b97cbfd4bfd564
Hai Vu <haivu2004@gmail.com>
2026-02-09 22:41:56 -0800

    feat: Add branch to prompt

--- startup/prompt.sh
+++ startup/prompt.sh
@@ -14,6 +14,17 @@ then
     )
 elif is_zsh
 then
+    # 1. Load the version control system module
+    autoload -Uz vcs_info
+    setopt PROMPT_SUBST
+
+    # 2. Run vcs_info before every prompt display
+    precmd() { vcs_info }
+
+    # 3. Style the output: (branch_name) with a space after it
+    zstyle ':vcs_info:git:*' formats '[%b] '
+
+    # 4. Your updated prompt
     start=196
-    PROMPT="%{$(tput setaf $start)%}%n%{$(tput setaf $((start + 6)))%}@%{$(tput setaf $((start + 12)))%}%m %{$(tput setaf $((start + 18)))%}%~ %{$(tput sgr0)%}"$'\n'"$ "
+    PROMPT=$'\n'"%? $(tput setaf $start)\${vcs_info_msg_0_}%{$(tput setaf $((start + 6)))%}%n%{$(tput setaf $((start + 12)))%}@%{$(tput setaf $((start + 18)))%}%m %{$(tput setaf $((start + 24)))%}%~ %{$(tput sgr0)%}"$'\n'"%# "
 fi
