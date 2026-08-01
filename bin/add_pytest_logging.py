a55090a1d161ff76bb4a640dccccdda68c0f5fe4
Hai Vu <haivu2004@gmail.com>
2026-07-31 19:51:59 -0700

    chore: reduce the amount of logging

--- bin/add_pytest_logging.py
+++ bin/add_pytest_logging.py
@@ -7,24 +7,8 @@ options = """
 
 [tool.pytest.ini_options]
 log_cli = true
-log_cli_level = "WARNING"
+log_cli_level = "DEBUG"
 
-log_file = "logs/my.log"
-log_file_level = "WARNING"
-
-# log_auto_indent (string)
-# log_cli (bool):                Enable log display during test run
-# log_cli_date_format (string)
-# log_cli_format (string)
-# log_cli_level (string)
-# log_date_format (string)
-# log_file_date_format (string)
-# log_file_format (string)
-# log_file_level (string)
-# log_file_mode (string)
-# log_file (string):             Default value for --log-file
-# log_format (string):           Default value for --log-format
-# log_level (string):            Default value for --log-level
 """
 
 
